ef11985ff693612efe9165aa2d76ca6b98f8aa2e
Namrata Kasaraneni <nammy.kasaraneni@gmail.com>
2022-03-07 12:08:36 -0800

    fixed bugs with interface, rerunning hyperparameter tuning with proper data splits

--- randomForestInterface.py
+++ randomForestInterface.py
@@ -16,17 +16,16 @@ class RandomForest(RandomForestBackbone):
         if not hyperparameters:
             hyperparameters = RandomForestHyperparamters()
         self.forest = RandomForestBackbone(hyperparameters.num_trees, 
-                                            hyperparameters.num_random_features, 
+                                            hyperparameters.num_features, 
                                             hyperparameters.max_depth)
     
-    def train(self, train_X, train_Y, val_X, val_Y):
+    def train(self, train_X, train_y, val_X, val_y):
         """
         train and tune a random forest
         @train_X, @train_Y: training data and labels (np arrays)
         @val_X, @val_Y: validation data and labels, will be ignored
         """
-        df = pd.DataFrame(train_X.append(train_Y))
-        forest.makeForest(df)
+        self.forest.makeForest(self.makeAllData(train_X, train_y))
     
     def predict(self, test_X):
         """ 
@@ -37,6 +36,15 @@ class RandomForest(RandomForestBackbone):
         test_X = list(test_X)
         preds = []
         for data_point in test_X:
-            preds.append(forest.classifyInstance(data_point))
-        
+            preds.append(self.forest.classifyInstance(data_point))
         return np.array(preds)
+    
+    def accuracy(self, test_X, test_y):
+        return self.forest.getAccuracy(self.makeAllData(test_X, test_y))
+    
+    def makeAllData(self, data_X, data_y):
+        """
+        take np arrays of data and labels and make a pandas df
+        """
+        all_data = np.insert(data_X, 21, data_y, axis=1)
+        return pd.DataFrame(all_data, columns = [i for i in range(all_data.shape[1])])
